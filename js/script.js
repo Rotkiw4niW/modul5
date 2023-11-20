87fa217963b2c662b1878f0acf1294cf08d78fb0
Wiktor.Gebka <gwiktor11114@gmail.com>
2023-11-20 18:54:19 +0100

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 'use strict';
 const linksList_AllPosts = document.querySelectorAll('.titles a');
-
+const activeLinks = document.querySelectorAll('.titles a.active');
+const activeArticles = document.querySelectorAll('.posts .active');
 
 
 
 function titleClickHandler(event){
      /* remove class 'active' from all article links  */
-
+     for(let link of activeLinks){
+      console.log(link);
+      link.classList.remove("active");
+  }
      console.log(event);
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
-
+  for(let post of activeArticles){
+    console.log(post);
+    post.classList.remove("active");
+}
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -22,8 +29,6 @@ function titleClickHandler(event){
 }
 
 
-
 for(let link of linksList_AllPosts){
-    console.log(link);
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 };
