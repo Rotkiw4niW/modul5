4f5dcc7c83c5597dfa75f45d921b1a113461ed8d
Wiktor.Gebka <gwiktor11114@gmail.com>
2023-11-20 19:10:55 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 'use strict';
 const linksList_AllPosts = document.querySelectorAll('.titles a');
-const activeLinks = document.querySelectorAll('.titles a.active');
-const activeArticles = document.querySelectorAll('.posts .active');
 
 
 
 function titleClickHandler(event){
-     /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+const activeArticles = document.querySelectorAll('.posts .active');
+
+  const clickedElement = this;
+     /*[DONE] remove class 'active' from all article links  */
      for(let link of activeLinks){
       console.log(link);
-      link.classList.remove("active");
+      link.classList.remove('active');
   }
      console.log(event);
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [IN POROGRES]add class 'active' to the clicked link */
+console.log('clickedElement', clickedElement);
+clickedElement.classList.add('active')
+  /* [DONE]remove class 'active' from all articles */
   for(let post of activeArticles){
     console.log(post);
-    post.classList.remove("active");
+    post.classList.remove('active');
 }
   /* get 'href' attribute from the clicked link */
 
