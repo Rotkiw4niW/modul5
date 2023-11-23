6853d3ea100e21fce0b5fefd0ee024e47aef2451
Wiktor.Gebka <gwiktor11114@gmail.com>
2023-11-23 23:19:54 +0100

    get title for list innerHTML [DONE]

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 const linksList_AllPosts = document.querySelectorAll('.titles a');
 
 
-
 function titleClickHandler(event){
-  event.preventDefault();
+  console.log("ki");
+  event.preventDefault(); //blokowanie domyślnej akcji (nie przeładowuje strony)
   const activeLinks = document.querySelectorAll('.titles a.active');
 const activeArticles = document.querySelectorAll('.posts .active');
 
@@ -26,13 +26,45 @@ clickedElement.classList.add('active')
   /*[DONE] get 'href' attribute from the clicked link */
 const linkHrefAtribute = this.getAttribute("href").replace(/[#]/g, '');
 console.log(linkHrefAtribute);
-  /* [IN PROGRES]find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE]find the correct article using the selector (value of 'href' attribute) */
 const clickedArticle = document.getElementById(linkHrefAtribute);
-  /* add class 'active' to the correct article */
+  /* [DONE]add class 'active' to the correct article */
 clickedArticle.classList.add('active');
 
 }
 
+
+const AllArticles = document.querySelectorAll('.post');
+const titleElements = document.querySelectorAll('.post-title');
+
+
+function generateTitleLinks(){
+// [DONE] delate content from links list
+for(let link of linksList_AllPosts){
+  link.innerHTML="";
+
+  
+  // [DONE]get "id" attribute from each article 
+  for(let post of AllArticles){
+    let ArticleId = post.getAttribute('id');
+    console.log(ArticleId);
+    
+  }
+  
+  // [DONE]find element with title & get innerHTML
+  for(let title of titleElements){
+    let titleFromElement = title.innerHTML;
+    console.log(titleFromElement);
+    
+    //[IN PROGRES] create "postTitle" const with htlm for titles
+    let HtmlPostTitle = titleFromElement;
+    console.log("tytuł" + HtmlPostTitle);
+  }
+// inject postTitle too list elements
+
+  }
+
+}
 
 for(let link of linksList_AllPosts){
   link.addEventListener('click', titleClickHandler);
